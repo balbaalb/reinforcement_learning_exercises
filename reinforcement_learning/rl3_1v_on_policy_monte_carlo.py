8c4b7d4a20664f59e63147e3acedd61324a94585
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2025-11-10 23:06:18 -0800

    Adding rl 3

---
+++ reinforcement_learning/rl3_1v_on_policy_monte_carlo.py
@@ -0,0 +1,40 @@
+from models.monte_carlo_methods import *
+from environments.mdp_slippery_frozen_lake import SlipperyFrozenLake
+from environments.bandit import Bandit
+import matplotlib.pyplot as plt
+
+"""
+Here the 1st-visit on-policy Monte Carlo method is used to improve win % for slippery frozen lake.
+Outcome:
+Episode 10000: win ratio = 50.8 %
+"""
+
+
+def main() -> None:
+    np.random.seed(45)
+    slip = 0.33  # 1.0 / 3.0
+    size = 4
+    hole_pos = [5, 7, 11, 12]
+    env = SlipperyFrozenLake(size=size, hole_pos=hole_pos, slip=slip)
+    optimal_policy, win_ratios = monte_carlo_control_1v_on(
+        env=env,
+        n_episodes=10000,
+        gamma=0.999,
+        verbose_frequency=100,
+        epsilon_start=1,
+        epsilon_decay=0.999,
+    )
+
+    for s in range(env.n_states):  # states_with_optimal_move:
+        print(f"pi({s}) = {[optimal_policy(s , a) for a in range(4)]}")
+    plt.plot(win_ratios)
+    plt.xlabel("Episode")
+    plt.ylabel("Total Win %")
+    plt.title("First-visit on-policy Monte Carlo control\nfor slippery frozen lake")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+# py -m reinforcement_learning.rl3_1v_on_policy_monte_carlo
