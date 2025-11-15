ec08b35e767a536593f3201adc48cced32f3d200
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2025-11-15 09:10:47 -0800

    adding None-initialized variables to Environment interface

--- environments/environment.py
+++ environments/environment.py
@@ -4,6 +4,16 @@ import numpy as np
 
 
 class Environment(ABC):
+    def __init__(self):
+        super().__init__()
+        self.state = None
+        self.reward = None
+        self.n_states = None
+        self.n_actions = None
+        self.step_number = None
+        self.max_steps = None
+        self.done = None
+
     @abstractmethod
     def reset(self) -> None:
         pass
