7d9c5d7b857e0a23f5402d2d57efdf999a602375
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2025-11-15 08:42:55 -0800

    moving play_env and gen_trajectory to run_policy.py

--- models/monte_carlo_methods.py
+++ models/monte_carlo_methods.py
@@ -1,36 +1,6 @@
-import pandas as pd
 import numpy as np
 from environments.environment import Environment
-from environments.run_policy import run_policy, PolicyType
-
-
-def play_env(env: Environment, n_episodes: int, policy: PolicyType):
-    total_rewards = 0
-    for _ in range(n_episodes):
-        env.reset()
-        while not env.done and env.step_number < env.max_steps:
-            a = run_policy(policy, state=env.state, n_actions=env.n_actions)
-            env.step(a)
-            total_rewards += env.reward
-    win_percent = total_rewards / n_episodes * 100
-    return win_percent
-
-
-def gen_trajectory(env: Environment, policy: PolicyType, gamma: float = 0):
-    env.reset()
-    trajectory = pd.DataFrame(columns=["state", "action", "reward"])
-    while not env.done and env.step_number < env.max_steps:
-        env.step_number += 1
-        prev_state = env.state
-        action = run_policy(policy=policy, state=prev_state, n_actions=env.n_actions)
-        env.step(action)
-        trajectory.loc[len(trajectory)] = [prev_state, action, env.reward]
-    gains = np.zeros(len(trajectory))
-    rewards = trajectory["reward"].values
-    for i in range(len(trajectory) - 1, -1, -1):
-        gains[i] = rewards[i] + gamma * (gains[i + 1] if i < len(trajectory) - 1 else 0)
-    trajectory["gain"] = gains
-    return trajectory
+from environments.run_policy import PolicyType, play_env, gen_trajectory
 
 
 def monte_carlo_control_1v_on(
