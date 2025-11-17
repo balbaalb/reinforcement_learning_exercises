0084ccbf09acb7784dcd371a644136b2e07baeb0
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2025-11-16 22:19:06 -0800

    Test for td_control

---
+++ test/test_td_methods.py
@@ -0,0 +1,32 @@
+from models.td_methods import *
+from environments.mdp_random_walk import RandomWalk
+
+
+def test_td_control():
+    np.random.seed(123)
+    env = RandomWalk(n_states=5, start_state=2, slip=0.4)
+    optimal_policy, _ = td_control(
+        env=env,
+        n_episodes=100,
+        gamma=0.99,
+        epsilon_start=1.0,
+        epsilon_decay=0.999,
+        alpha_start=1.0,
+        alpha_decay=0.9,
+        on_policy=False,
+    )
+    for s in range(1, env.n_states - 1):  # states_with_optimal_move:
+        assert optimal_policy(s, 0) < optimal_policy(s, 1)
+
+    optimal_policy, _ = td_control(
+        env=env,
+        n_episodes=300,
+        gamma=0.99,
+        epsilon_start=1.0,
+        epsilon_decay=0.999,
+        alpha_start=1.0,
+        alpha_decay=0.999,
+        on_policy=True,
+    )
+    for s in range(1, env.n_states - 1):  # states_with_optimal_move:
+        assert optimal_policy(s, 0) < optimal_policy(s, 1)
