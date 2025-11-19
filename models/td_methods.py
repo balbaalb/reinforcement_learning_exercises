4980d40cfc61542fdc6e849721718fc3e24658ed
Bashar Albaalbaki <bashar.albaalbaki@gmail.com>
2025-11-19 07:31:44 -0800

    SARSA(λ) and Watkin's Q(λ) blueprint

--- models/td_methods.py
+++ models/td_methods.py
@@ -11,11 +11,13 @@ def td_control(
     epsilon_decay: float = 0.99,
     alpha_start: float = 1.0,
     alpha_decay: float = 0.99,
+    lambda_td: float = 0,
     verbose_frequency: int | None = None,
     on_policy: bool = False,
 ) -> PolicyType:
     """
-    Control method that uses one step TD-learning. Inputs:
+    Control method that uses one step TD-learning.
+    Eligibility trace type: accumulating traces
     """
     policy = lambda s, a: 1.0 / env.n_actions
     q = np.zeros([env.n_states, env.n_actions], dtype=float)
@@ -28,6 +30,7 @@ def td_control(
             win_ratios.append(play_env(env=env, n_episodes=1000, policy=policy))
             print(f"Episode {episode + 1}: win ratio= {round(win_ratios[-1], 2)} %")
         env.reset()
+        E = np.zeros_like(q)
         while not env.done and env.step_number < env.max_steps:
             s0 = env.state
             a0 = run_policy(policy=policy, state=s0, n_actions=env.n_actions)
@@ -38,9 +41,18 @@ def td_control(
                 a1 = run_policy(policy=policy, state=s1, n_actions=env.n_actions)
             else:  # Q-learning
                 a1 = a_optimum[s1]  # if a1 comes from policy this becomes sars
+                if lambda_td > 1.0e-10:
+                    ap = run_policy(policy=policy, state=s1, n_actions=env.n_actions)
             g = r + gamma * q[s1, a1]
             diff = g - q[s0, a0]
             q[s0, a0] += alpha * diff
+            if lambda_td > 1.0e-10:
+                q += alpha * diff * E
+                if on_policy or ap == a1:
+                    E[s0, a0] += 1
+                    E = gamma * lambda_td * E
+                else:
+                    E = 0
             a_optimum[s0] = np.argmax(q[s0, :])
             policy = lambda s, a: epsilon / env.n_actions + (
                 (1.0 - epsilon) if a == np.argmax(q[s, :]) else 0
